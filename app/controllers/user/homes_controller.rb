a0d439859762e883cf60420dc5c56d29dc4a3788
Jai Rajput <jai@tecorb.co>
2025-02-18 16:24:23 +0530

    all changes and add devices

--- app/controllers/user/homes_controller.rb
+++ app/controllers/user/homes_controller.rb
@@ -1,4 +1,6 @@
 class User::HomesController < ApplicationController
+  # before_action :authenticate_user!
+  
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'user/homes#index'
-
+  devise_for :users
   namespace :user do
     get 'homes/index'
   end
